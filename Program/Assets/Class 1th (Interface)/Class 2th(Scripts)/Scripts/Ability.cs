2c162ab00617963f68df09e1995c612befd0328a
DOBBY0901 <ldjshfkd0901@gmail.com>
2025-09-12 21:43:58 +0900

    (2025.09.12) 수출용 패키지

---
+++ Program/Assets/Class 1th (Interface)/Class 2th(Scripts)/Scripts/Ability.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[System.Serializable]
+public class Ability
+{
+    [SerializeField] int strength;
+    [SerializeField] int dexterity;
+    [SerializeField] int intelligence;
+    [SerializeField] int wisdom;
+}
