0569d11066cd4beb2f70769b0fe393fb266542cc
DOBBY0901 <ldjshfkd0901@gmail.com>
2025-09-16 21:42:51 +0900

    (2025 09 16) 트랜스폼

---
+++ Program/Assets/Class 5th(Transform)/Scripts/Pilot.cs
@@ -0,0 +1,17 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Pilot : MonoBehaviour
+{
+   
+    void Start()
+    {
+        transform.position += new Vector3(0, 0, 1);
+    }
+
+   
+    void Update()
+    {
+       
+    }
+}
