f97f2547c9315bd59fc90142289bfc949dac6688
DOBBY0901 <ldjshfkd0901@gmail.com>
2025-09-17 21:42:24 +0900

    (2025 09 17 UI 맛보기)

--- Program/Assets/Class 5th(Transform)/Scripts/Pilot.cs
+++ Program/Assets/Class 5th(Transform)/Scripts/Pilot.cs
@@ -3,15 +3,26 @@ using UnityEngine;
 
 public class Pilot : MonoBehaviour
 {
-   
+    [SerializeField] float speed;
+    [SerializeField] Vector3 direction;
     void Start()
     {
-        transform.position += new Vector3(0, 0, 1);
+       
     }
 
    
     void Update()
     {
-       
+        direction.x = Input.GetAxisRaw("Horizontal");
+        direction.z = Input.GetAxisRaw("Vertical");
+
+        direction.Normalize();
+
+        //Time.deltatime
+        //이전 프레임 완료시점부터 현재 프레임이 시작되기까지 지난 시간을 반환
+        
+        transform.position = transform.position + direction * speed * Time.deltaTime;
+        
+
     }
 }
