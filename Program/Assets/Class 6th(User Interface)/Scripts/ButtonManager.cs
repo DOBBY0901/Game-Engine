e353d4024c100e7a02b65d45718af20aa5a867ce
DOBBY0901 <ldjshfkd0901@gmail.com>
2025-09-18 21:43:10 +0900

    (2025. 09. 18 유저 인터페이스)

---
+++ Program/Assets/Class 6th(User Interface)/Scripts/ButtonManager.cs
@@ -0,0 +1,46 @@
+using JetBrains.Annotations;
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour
+{
+    [SerializeField] string[] titleList;
+    [SerializeField] Button[] buttonList;
+
+    void Start()
+    {
+        Action[] actions = { Execute, Option, Quit };
+
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            int index = i;
+
+            buttonList[i].onClick.AddListener(() => actions[index]());
+
+            buttonList[i].GetComponentInChildren<Text>().text = titleList[index];
+
+        }
+
+
+    }
+    public void Execute()
+    {
+        Debug.Log("Execute");
+    }
+     public void Option()
+    {
+        Debug.Log("Option");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit");
+    }
+   
+
+
+}   
+
