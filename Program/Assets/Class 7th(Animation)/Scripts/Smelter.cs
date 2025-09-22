70fb929f45bc381bdf2c857551b397968bab2836
DOBBY0901 <ldjshfkd0901@gmail.com>
2025-09-22 21:43:51 +0900

    (2025 09 22) 애니메이터 컨트롤러 시스템

--- Program/Assets/Class 7th(Animation)/Scripts/Smelter.cs
+++ Program/Assets/Class 7th(Animation)/Scripts/Smelter.cs
@@ -2,8 +2,28 @@ using UnityEngine;
 
 public class Smelter : MonoBehaviour
 {
-   public void Success()
+    [SerializeField] float progress;
+    [SerializeField] int Count;
+    public void Sucess()
     {
-        Debug.Log("Success");
+        Debug.Log("Creat...");
     }
+    
+    public void Process(float progress)
+    {
+
+        this.progress += progress;
+
+        Debug.Log("Progress : " + this.progress + "%");
+
+        this.progress = this.progress % 100;
+    }
+
+    public void Enhance(int count)
+    {
+        this.Count += count;
+
+        Debug.Log("Enhance Count : " + this.Count);
+    }
+
 }
