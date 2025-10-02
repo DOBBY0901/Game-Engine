d8cf7f6833d724a74d7b2965d1c054276742a065
DOBBY0901 <ldjshfkd0901@gmail.com>
2025-10-02 21:43:26 +0900

    (20251002 광선검출 시스템)

---
+++ Program/Assets/Class 12th(Ray Cast)/Scripts/Mouse.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Mouse : MonoBehaviour
+{
+    [SerializeField] Ray ray;
+    [SerializeField] RaycastHit rayCastHit;
+    [SerializeField] float distance;
+    [SerializeField] Texture2D texture2D;
+
+
+    private void Awake()
+    {
+        distance = Mathf.Infinity;
+
+        Cursor.SetCursor(texture2D,Vector2.zero, CursorMode.Auto);    
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+           ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            Debug.DrawRay(ray.origin, ray.direction * distance, Color.green);
+
+            if (Physics.Raycast(ray, out rayCastHit, 100))
+            {
+                Debug.Log(rayCastHit.collider.name);
+
+                Debug.DrawLine(ray.origin, rayCastHit.point, Color.red);
+            }
+        }
+    }
+}
